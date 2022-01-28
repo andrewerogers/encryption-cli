20e26d9fa27b69bd185f20344c4c25a0c9112f0a
adrewrog <adrewrog@adrewrog-desktop.localdomain>
2022-01-27 19:58:53 -0600

    Move to encryption using AES-GCM

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # encryption-cli
 
-Simple encryption CLI tool using caesar cipher. 
+Simple encryption cli using AES-GCM. This package uses a nil padding nonce so passwords
+are reproducible. In production-grade applications, nonce should be randomly generated. 
--- main.go
+++ main.go
@@ -2,27 +2,47 @@ package main
 
 import (
 	"bufio"
+	"crypto/aes"
+	"crypto/cipher"
 	"fmt"
 	"os"
+)
+
+const(
+	key = "abcdefghijklmnopqrstuvwxyz012345"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	for {
-		fmt.Print("Enter your password: ")
+		fmt.Print("> Enter your password: ")
 		in, _ := reader.ReadString('\n')
-		out, err := encrypt(in)
-
+		out, err := encrypt(in, []byte(key))
 		if err != nil {
-			fmt.Printf("> An error occurred")
-			break
+			fmt.Println(err)
 		}
 
-		fmt.Printf("> Your encrypted pasword is %s\n", out)
+		fmt.Printf("> Your encrypted password is %s\n", out)
 	}
 }
 
-func encrypt(s string) (string, error) {
-	// todo: research open source libraries to use for this. 
-	return "", nil
+// encrypt will encrypt plaintext using AES-GCM
+// - Requires AES-128 or AES-256 keys
+// - Uses nil padding nonce for convenience
+func encrypt(data string, key []byte) (string, error) {
+	plaintext := []byte(data)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	nonce := make([]byte, 12)
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	ciphertext := gcm.Seal(nil, nonce, plaintext, nil)
+	return string(ciphertext), nil
 }
